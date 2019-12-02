7f710682c0385e865a5c25627163f3a9ab292be8
Pavel Khmelyov <pkhmelyov@gmail.com>
2019-12-02 22:49:43 +0300

    Sending post into selected channel (still not finished)

--- src/pkhmelyov.AbpReposterBot.Web.Mvc/Models/Posts/PostsSendViewModel.cs
+++ src/pkhmelyov.AbpReposterBot.Web.Mvc/Models/Posts/PostsSendViewModel.cs
@@ -7,10 +7,12 @@ namespace pkhmelyov.AbpReposterBot.Web.Mvc.Models.Posts
     public class PostsSendViewModel
     {
         [Required]
-        public int Id { get; set; }
+        public long Id { get; set; }
 
         public PostListDto Post { get; set; }
 
         public PagedResultDto<ChannelDto> Channels { get; set; }
+
+        public long ChannelId { get; set; }
     }
 }
